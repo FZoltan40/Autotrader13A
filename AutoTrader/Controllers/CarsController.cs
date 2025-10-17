9bc7650999c7f620c1f34bbb885c1ddc3c6f77ba
FZoltan40 <farkasz@kkszki.hu>
2025-10-17 11:48:14 +0200

    GetAll enpoint is ready to use.

---
+++ AutoTrader/Controllers/CarsController.cs
@@ -0,0 +1,27 @@
+﻿using AutoTrader.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AutoTrader.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarsController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<Car> GetAllRecord()
+        {
+            using (var context = new CarDbContext())
+            {
+                var cars = context.Cars.ToList();
+
+                if (cars != null)
+                {
+                    return Ok(cars);
+                }
+
+                return BadRequest(new { message = "Sikertelen lekérdezés." });
+            }
+
+        }
+    }
+}
